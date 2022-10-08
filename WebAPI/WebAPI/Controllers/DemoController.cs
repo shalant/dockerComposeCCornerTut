2d6bbf23ab719821e37fae0aa669a5d292d22027
shalant <doug.rosenberg@gmail.com>
2022-10-08 11:02:41 -0500

    initial

---
+++ WebAPI/WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("demo")]
+
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Docker World";
+        }
+    }
+}
---
+++ WebAPP/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import configurl from '../assets/config.json';
+import { DemoService } from './demo.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  config = {
+    ApiUrl: configurl.apiServer.url
+  };
+  title: 'WebApp';
+  response: 'No data loaded, yet';
+
+  constructor(private sharedService: DemoService){}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.refreshList();
+    }, 2000);
+  }
+
+  refreshList() {
+    this.sharedService.getData().subscribe(data => {
+      console.log(data)
+      this.response = data;
+    });
+  }
+}
